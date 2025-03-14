0f626c4b68d346024bdf4074ae104d94b7a98b4a
nguyenthang <Tl291095@>
2025-03-14 09:58:32 +0700

    Update locator

--- src/main/java/pages/ios/seller/home/HomeElement.java
+++ src/main/java/pages/ios/seller/home/HomeElement.java
@@ -1,9 +1,10 @@
 package pages.ios.seller.home;
 
+import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
 
 public class HomeElement {
-    By loc_icnAccount = By.xpath("//XCUIElementTypeImage[@name=\"icon_tabbar_account\"]/parent::XCUIElementTypeButton");
+    By loc_icnAccount = AppiumBy.iOSNsPredicateString("name == \"Tài khoản\" or name == \"Account\"");
     By loc_icnCreateProduct = By.xpath("//*[XCUIElementTypeImage[@name=\"icon_home_create_new_product\"]]/XCUIElementTypeButton");
     By loc_icnProductManagement = By.xpath("//XCUIElementTypeImage[@name=\"icon_home_product_management\"]/preceding-sibling::XCUIElementTypeButton");
     By loc_icnSupplierManagement = By.xpath("//*[*[@name=\"icon_home_suplier\"]]/XCUIElementTypeButton");
